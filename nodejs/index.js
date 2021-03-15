b9ee31acde16170c120302eabfe35e2944dbad68
Sarahi Estevane <sarauez98@gmail.com>
2021-03-14 23:08:09 -0600

    Creacion NODEJS, post, put

---
+++ nodejs/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const requestHandler = require('./request-handler');
+const recursos = require('./recursos');
+
+global.recursos = recursos;
+
+const server = http.createServer(requestHandler);
+
+server.listen(5000 , ()=>{
+    console.log('el servidor esta escuchando peticiones en http://localhost:5000/');
+});

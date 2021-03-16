cc7dd4d6f099efc9d410bacb225e73f01397ee69
Sarahi Estevane <sarauez98@gmail.com>
2021-03-15 23:29:56 -0600

    Creacion CRUDL mascotas, dueños y vet

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -1,38 +1,16 @@
+const recursos = require('./recursos');
+const mascotas = require('./rutas/mascotas');
+const veterinarias = require('./rutas/veterinarias');
+const duenos = require('./rutas/duenos');
+
 module.exports = {
     ruta: (data, callback) => {
         callback(200, {mensaje: "esta es /ruta"});
     },
-    mascotas: {
-        get: (data, callback) => {
-            if(typeof data.indice !=="undefined"){
-                console.log("handler mascotas ", {data});
-                if(global.recursos.mascotas[data.indice]){
-                    return callback(200,global.recursos.mascotas[data.indice]);
-                }
-                return callback(404,
-                    {mensaje: `mascota con indice ${data.indice} no encontrada`,
-                });
-            }
-            callback(200, global.recursos.mascotas);
-        },
-        post: (data, callback) => {
-            global.recursos.mascotas.push(data.payload);
-            callback(201, data.payload);
-        },
-        put: (data, callback) => {
-            if(typeof data.indice !=="undefined"){
-                if(global.recursos.mascotas[data.indice]){
-                    global.recursos.mascotas[data.indice] =data.payload;
-                    return callback(200,global.recursos.mascotas[data.indice]);
-                }
-                return callback(404,
-                    {mensaje: `mascota con indice ${data.indice} no encontrada`,
-                });
-            }
-            callback(400, {mensaje: "indice no enviado"} );
-        },
-    },
+    mascotas: mascotas(recursos.mascotas),
+    veterinarias: veterinarias(recursos.veterinarias),
+    duenos: duenos(recursos.duenos),
     noEncontrado: (data, callback) => {
         callback(404, {mensaje: "no encontrado"});
-    }
-}
+    },
+};

54535ecfbe8aad7e03dcd9e74d414452faabe98f
Sarahi Estevane <sarauez98@gmail.com>
2021-03-20 18:03:39 -0600

    CRUD desde el frontend veterinarias/duenos

--- veterinaria-fullstack/frontend/JS/duenos.js
+++ veterinaria-fullstack/frontend/JS/duenos.js
@@ -1,33 +1,26 @@
 const listaDuenos = document.getElementById('lista-duenos');
-const tipo = document.getElementById('pais');
 const nombre = document.getElementById('nombre');
-const identificacion = document.getElementById('identificacion');
+const documento = document.getElementById('documento');
 const apellido = document.getElementById('apellido');
 const indice =document.getElementById('indice');
 const form = document.getElementById('form');
 const btnguardar = document.getElementById('btn-guardar');
 const modalh5 = document.getElementById('exampleModalLongTitle');
+const url = 'http://localhost:5000/duenos';
 
-let duenos =[
-    {
-        nombre: "Emiliano",
-        apellido: "Soto",
-        pais: "Colombia",
-        identificacion: "1234567890"
-    },
-    {
-        nombre: "Laura",
-        apellido: "Fuentes",
-        pais: "Mexico",
-        identificacion: "1234567810"
+let duenos = [];
+
+async function listarDuenos() {
+  try {
+    const respuesta = await fetch(url);
+  const duenosDelServer = await respuesta.json();
+  if (Array.isArray(duenosDelServer)){
+    duenos = duenosDelServer;
   }
-];
-
-function listarDuenos() {
-    let htmlDuenos = duenos.map((dueno, index)=>` <tr>
+  if(duenos.length > 0){
+  const htmlDuenos = duenos.map((dueno, index)=>` <tr>
     <th scope="row">${index}</th>
-    <td>${dueno.identificacion}</td>
-    <td>${dueno.pais}</td>
+    <td>${dueno.documento}</td>
     <td>${dueno.nombre}</td>
     <td>${dueno.apellido}</td>
     <td>
@@ -38,45 +31,80 @@ function listarDuenos() {
     </td>
   </tr>`).join("");
   listaDuenos.innerHTML= htmlDuenos;
-  Array.from(document.getElementsByClassName('editar')).forEach((botonEditar,index)=>botonEditar.onclick = editar(index))
-  Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar,index)=>botonEliminar.onclick = eliminar(index))
+  Array.from(document.getElementsByClassName('editar')).forEach(
+    (botonEditar,index)=>(botonEditar.onclick = editar(index))
+  );
+  Array.from(document.getElementsByClassName('eliminar')).forEach(
+    (botonEliminar,index)=>(botonEliminar.onclick = eliminar(index))
+  );
+  return;
+  }
+  listaDuenos.innerHTML= `<tr>
+    <td colspan="5">No hay dueñ@s</td>
+  </tr>`;
+  } catch (error) {
+    console.log({error});
+    $(".alert").show();
+  }
 }
 
-function enviarDatos(evento) {
+async function enviarDatos(evento) {
   evento.preventDefault();
-  const datos = {
-        nombre: nombre.value,
-        apellido: apellido.value,
-        pais: pais.value,
-        identificacion: identificacion.value
-};
-  const accion = btnguardar.innerHTML;
-  switch(accion){
-    case 'Editar':
-      duenos[indice.value] = datos;
-     break;
-      default:
-        duenos.push(datos);
-      break;
+  try {
+    const datos = {
+      nombre: nombre.value,
+      apellido: apellido.value,
+      documento: documento.value
+    };
+    const accion = btnguardar.innerHTML;
+    let urlEnvio = url;
+    let method = "POST";
+    if(accion == "Editar"){
+      urlEnvio +=  `/${indice.value}`;
+      method = "PUT";
+    }
+    const respuesta = await fetch(urlEnvio, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(datos),
+      mode: "cors",
+    });
+      if(respuesta.ok){
+        listarDuenos();
+        resetModal();
+      }
+  } catch (error) {
+    console.log({error});
+    $(".alert").show();
   }
-  listarDuenos();
-    resetModal();
 }
 
 function resetModal(){
     indice.value = '';
     nombre.value = '';
     apellido.value= '';
-    pais.value= '';
-    identificacion.value = '';
+    documento.value = '';
     btnguardar.innerHTML='Crear';
     modalh5.innerHTML='Nuevo Dueño';
 }
 
 function eliminar(index){
-  return function clicEnEliminar(){
-    duenos = duenos.filter((dueno, indiceDueno)=>indiceDueno !== index);
-    listarDuenos();
+  const urlEnvio = `${url}/${index}`;
+  return async function clicEnEliminar(){
+    try {
+      const respuesta = await fetch(urlEnvio, {
+        method: 'DELETE',
+        mode: "cors",
+      });
+        if(respuesta.ok){
+          listarDuenos();
+        }
+    } catch (error) {
+      console.log({error});
+      $(".alert").show();
+    }
   }
 }
 
@@ -89,8 +117,7 @@ function editar(index){
     indice.value = index;
     nombre.value = dueno.nombre;
     apellido.value= dueno.apellido;
-    pais.value= dueno.pais;
-    identificacion.value = dueno.identificacion;
+    documento.value = dueno.documento;
   }
 }
 
--- veterinaria-fullstack/frontend/JS/veterinarias.js
+++ veterinaria-fullstack/frontend/JS/veterinarias.js
@@ -1,7 +1,6 @@
 const listaVeterinarias = document.getElementById('lista-veterinarias');
-const tipo = document.getElementById('pais');
 const nombre = document.getElementById('nombre');
-const identificacion = document.getElementById('identificacion');
+const documento = document.getElementById('documento');
 const apellido = document.getElementById('apellido');
 const indice =document.getElementById('indice');
 const form = document.getElementById('form');
@@ -14,12 +13,12 @@ let veterinarias =[];
 async function listarVeterinarias() {
   try {
     const respuesta = await fetch(url);
-  const veterinariasDelServer = await respuesta.json();
-  if (Array.isArray(veterinariasDelServer)){
-    veterinarias = veterinariasDelServer;
-  }
-  if(veterinarias.length > 0){
-  const htmlVeterinarias = veterinarias.map((veterinaria, index)=>` <tr>
+    const veterinariasDelServer = await respuesta.json();
+    if (Array.isArray(veterinariasDelServer)){
+      veterinarias = veterinariasDelServer;
+    }
+    if(veterinarias.length > 0){
+    const htmlVeterinarias = veterinarias.map((veterinaria, index)=>` <tr>
     <th scope="row">${index}</th>
     <td>${veterinaria.documento}</td>
     <td>${veterinaria.nombre}</td>
@@ -33,56 +32,78 @@ async function listarVeterinarias() {
     </tr>`).join("");
     listaVeterinarias.innerHTML= htmlVeterinarias;
     Array.from(document.getElementsByClassName('editar')).forEach(
-    (botonEditar,index)=>(botonEditar.onclick = editar(index))
+      (botonEditar,index)=>(botonEditar.onclick = editar(index))
     );
     Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar,index)=>(botonEliminar.onclick = eliminar(index))
     );
     return;
-  }
-  listaVeterinarias.innerHTML= `<tr>
+    }
+    listaVeterinarias.innerHTML= `<tr>
     <td colspan="5">No hay veterinarias</td>
-  </tr>`;
+    </tr>`;
+  }catch (error) {
+    console.log({error});
+    $(".alert").show();
+  }
+}
+
+async function enviarDatos(evento) {
+  evento.preventDefault();
+  try {
+    const datos = {
+      nombre: nombre.value,
+      apellido: apellido.value,
+      documento: documento.value
+};
+const accion = btnguardar.innerHTML;
+let urlEnvio = url;
+let method = "POST";
+if(accion === 'Editar'){
+  urlEnvio +=  `/${indice.value}`;
+  method = "PUT";
+}
+const respuesta = await fetch(urlEnvio, {
+  method,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(datos),
+  mode: "cors",
+});
+  if(respuesta.ok){
+    listarVeterinarias();
+    resetModal();
+  }
   } catch (error) {
     console.log({error});
     $(".alert").show();
   }
 }
 
-function enviarDatos(evento) {
-  evento.preventDefault();
-  const datos = {
-        nombre: nombre.value,
-        apellido: apellido.value,
-        pais: pais.value,
-        identificacion: identificacion.value
-};
-  const accion = btnguardar.innerHTML;
-  switch(accion){
-    case 'Editar':
-      veterinarias[indice.value] = datos;
-     break;
-      default:
-        veterinarias.push(datos);
-      break;
-  }
-  listarVeterinarias();
-    resetModal();
-}
-
 function resetModal(){
     indice.value = '';
     nombre.value = '';
     apellido.value= '';
-    pais.value= '';
-    identificacion.value = '';
+    documento.value = '';
     btnguardar.innerHTML='Crear';
     modalh5.innerHTML='Nuevo Veterinario';
 }
 
 function eliminar(index){
-  return function clicEnEliminar(){
-    veterinarias = veterinarias.filter((veterinaria, indiceVeterinaria)=>indiceVeterinaria !== index);
-    listarVeterinarias();
+  const urlEnvio = `${url}/${index}`;
+  return async function clicEnEliminar(){
+    try {
+      const respuesta = await fetch(urlEnvio, {
+        method: 'DELETE',
+        mode: "cors",
+      });
+        if(respuesta.ok){
+          listarVeterinarias();
+        }
+    } catch (error) {
+      console.log({error});
+      $(".alert").show();
+    }
   }
 }
 
@@ -95,8 +116,7 @@ function editar(index){
     indice.value = index;
     nombre.value = veterinaria.nombre;
     apellido.value= veterinaria.apellido;
-    pais.value= veterinaria.pais;
-    identificacion.value = veterinaria.identificacion;
+    documento.value = veterinaria.documento;
   }
 }
 

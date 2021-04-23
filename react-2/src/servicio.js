5b1e213f11795be8eddd6e199c2daba8c4fc8b3d
Sarahi Estevane <sarauez98@gmail.com>
2021-04-23 02:31:48 -0500

    cambios necesarios

--- react-2/src/servicio.js
+++ react-2/src/servicio.js
@@ -1,7 +1,7 @@
- const API_URL = "https://veterinaria-backend-orpin.vercel.app";
+const API_URL = "https://veterinaria-backend-orpin.vercel.app";
  
  
- export const listarEntidad = async ({entidad = "mascotas"}) => {
+export const listarEntidad = async ({entidad = "mascotas"}) => {
      try {
          const respuesta = await fetch(`${API_URL}/${entidad}`);
          const datos = await respuesta.json();
@@ -9,32 +9,37 @@
      } catch (error) {
          console.log({error});
      }
- };
-
- export const crearEditarEntidad = async ({entidad = "mascotas", objeto ={}, method = "POST", idObjeto = null}) => {
-    try {
-        let url = null; 
-        if (method === 'PUT' && idObjeto) {
-            url += `${API_URL}/${entidad}/${idObjeto}`;
-        }else if (method === 'POST'){
-            url = `${API_URL}/${entidad}`;
-        }
-        if(!url){
-            throw new Error ("No cumple criterios de envio");
-        }
-        const respuesta = await fetch(`${API_URL}/${entidad}`, {
-            method,
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(objeto),
-            mode: "cors",
-          });
-        const datos = await respuesta.json();
-        return datos;
-    } catch (error) {
-        console.log({error});
-    }
 };
+export const crearEditarEntidad = async ({
+    entidad = "mascotas",
+    objeto = {},
+    method = "POST",
+    idObjeto = null,
+  }) => {
+    try {
+      let url = null;
+      if (method === "PUT" && idObjeto) {
+        url += `${API_URL}/${entidad}/${idObjeto}`;
+      } else if (method === "POST") {
+        url = `${API_URL}/${entidad}`;
+      }
+      if (!url) {
+        throw new Error("No cumple criterios de envio");
+      }
+      const respuesta = await fetch(`${API_URL}/${entidad}`, {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(objeto),
+        mode: "cors",
+      });
+      const datos = await respuesta.json();
+      return datos;
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+ 
 
  

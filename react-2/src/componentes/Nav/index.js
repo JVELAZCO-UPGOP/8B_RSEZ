8dc3c5a49104d53918f2407eec28ca5cd6689fe0
Sarahi Estevane <sarauez98@gmail.com>
2021-04-24 22:44:29 -0500

    react router

--- react-2/src/componentes/Nav/index.js
+++ react-2/src/componentes/Nav/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import "./Nav.css"
 import Search from "../Search";
 
 function Nav() {
   return (<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-  <a className="navbar-brand" href="#">Veterinaria</a>
+        <Link className="navbar-brand" to="/">
+        Veterinaria
+        </Link>
   <div className="navbar-right " id="navbarColor03">
       <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-              <a className="nav-link" href="/index.htm">Mascotas <span className="sr-only">(current)</span></a>
+          <Link className="nav-link" to="/">
+              Mascotas<span className="sr-only">(current)</span>
+            </Link>
           </li>
           <li className="nav-item">
-              <a className="nav-link" href="/veterinarias.html">Veterinarios</a>
+          <Link className="nav-link" to="/veterinarias">
+              Veterinari@s
+            </Link>
           </li>
           <li className="nav-item">
-              <a className="nav-link" href="/consultas.html">Consultas</a>
+          <Link className="nav-link" to="/consultas">
+              Consultas
+            </Link>
           </li>
           <li className="nav-item">
-              <a className="nav-link" href="/duenos.html">Dueños</a>
+          <Link className="nav-link" to="/duenos">
+              Dueños
+            </Link>
           </li>
       </ul>
       <Search/>
